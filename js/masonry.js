7c323ae995c59abb60886a884e9272507bacc96e
Margeaux Spring <contact@cheapwebmonkey.com>
2015-06-09 15:52:33 -0400

    added resized images for gallery, updated gallery page with no jumbotron, updated my-styles.css with new button style

--- js/masonry.js
+++ js/masonry.js
@@ -1,19 +1,11 @@
 // external JS: masonry.pkgd.js
-
 $(document).ready( function() {
-  
-  var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 160
-  });
 
-  $grid.on( 'click', '.grid-item', function() {
-    // change size of item via class
-    $( this ).toggleClass('grid-item--gigante');
-    // trigger layout
-    $grid.masonry();
+  $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
   });
   
 });
-
 
