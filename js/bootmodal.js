f45461394841a3ae0ddd638af118bc394fdf7217
Margeaux Spring <contact@cheapwebmonkey.com>
2015-06-23 19:45:31 -0400

    fixed modal sizing from huge to norm

--- js/bootmodal.js
+++ js/bootmodal.js
@@ -3,5 +3,9 @@ $('.thumbnail').click(function(){
   	var title = $(this).parent('a').attr("title");
   	$('.modal-title').html(title);
   	$($(this).parents('div').html()).appendTo('.modal-body');
+  	$('#myModal').on('show.bs.modal', function () {
+    $('.modal .modal-body').css('overflow-y', 'auto'); 
+    $('.modal .modal-body').css('max-height', $(window).height() * 0.7);
+});
   	$('#myModal').modal({show:true});
 });
