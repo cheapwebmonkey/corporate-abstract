4b7177595a959a90eeb065376d2333af7285df07
Margeaux Spring <contact@cheapwebmonkey.com>
2015-08-01 10:18:10 -0400

    added font awesome link

--- js/masonry.js
+++ js/masonry.js
@@ -9,3 +9,15 @@ $(document).ready( function() {
   
 });
 
+$('.thumbnail').click(function(){
+  	$('.modal-body').empty();
+  	var title = $(this).parent('a').attr("title");
+  	$('.modal-title').html(title);
+  	$($(this).parents('div').html()).appendTo('.modal-body');
+  	$('#myModal').on('show.bs.modal', function () {
+    $('.modal .modal-body').css('overflow-y', 'auto'); 
+    $('.modal .modal-body').css('max-height', $(window).height() * 0.7);
+});
+  	$('#myModal').modal({show:true});
+});
+
